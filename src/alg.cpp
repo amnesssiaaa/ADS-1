023818ba4698120fb2ca3c432eea5ba3fd58f120
amnesssiaaa <zvezda15032007@gmail.com>
2026-03-01 19:13:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,40 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+  if (value <= 1) return false;
+  for (uint64_t i = 2; i < value; i++) {
+    if (value % i  == 0) return false;
+  }
+  return true;        
 }
 
-uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+uint64_t nPrime(uint64_t n) {    
+  uint64_t count = 0;
+  for (uint64_t i = 2; count < n; i++) {
+    if (checkPrime(i)) count++;
+    if (count == n) return i;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  value++;
+  for (uint64_t i = value; ; i++) {
+    if (checkPrime(i)) return i;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t summa = 0;
+  for (uint64_t i = 2; i < hbound; i++){
+    if (checkPrime(i)) summa += i; 
+  }
+  return summa;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t count = 0;
+  for (uint64_t i = lbound; (i+2) < hbound; i++) {
+    if (checkPrime(i) && checkPrime(i+2)) count++;
+  }
+  return count;
 }
